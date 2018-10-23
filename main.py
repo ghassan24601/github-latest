76c18dc468f8ebd0bc616bd729db74bdd34d1292
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-10-22 17:53:19 -0700

    user hw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import json
-
 import requests
 
 # Use Like python githubber.py JASchilz
@@ -8,13 +7,10 @@ import requests
 
 if __name__ == "__main__":
     username = sys.argv[1]
-
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
-    
-
+    response = requests.get(f"https://api.github.com/users/{username}/events")
+    try:
+        latest_event = response.json()[0]
+        print(f'Latest update from {username}: ' + latest_event['created_at'])
+    except TypeError:
+        print('Incorrect user')
 
